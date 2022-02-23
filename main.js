5a4b3de7b672c8369ed448ede75eb3f11ad61ff6
alessandrakf <alessandra.kinneyferraro@gmail.com>
2022-02-23 14:40:33 -0500

    Additional changes

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ function userPicked(buttonName) {
           // speakers.play();
           zImage.src = 'images/jess.png';
           zZodiac.innerHTML = "You are Jess!";
-          zDescription.innerHTML = "As the sensitive and intuitive sign, your characteristics align perfectly with Jess. She is kindhearted and wants the best for everyone around her, even if she at times is taken for granted. Your sole mission in life is to help those who are less fortunate and your warmhearted nature makes you stand out. Your ability to place yourself in anyone’s shoes is one of the things people cherish the most about you.";
+          zDescription.innerHTML = "As the sensitive and intuitive sign, your characteristics align perfectly with Jess. She is kindhearted and wants the best for everyone around her, even if she at times is taken for granted. Your sole mission in life is to help those who are less fortunate and your warmheart4ed nature makes you stand out. Your ability to place yourself in anyone’s shoes is one of the things people cherish the most about you.";
           modal.hidden = !modal.hidden;
       break;
       case 'Aries':
@@ -191,50 +191,7 @@ function userPicked(buttonName) {
   }
 }
 
-// add Event Listener
-userSubmitBtn.addEventListener('click', function(){
-    //console.log('Submit button clicked!')
-    const userBday = new Date(document.getElementById('birthdayInput')).ariaValueMax;
-    //console.log(userBday);
 
-    const whichMonth = userBday.getUTCMonth();
-    whichMonth++;
-    console.log(`Users month is: ${whichMonth}`);
-
-    const whichDayOfMonth = userBday.getUTCDate();
-    console.log(`Users birth day is: ${whichDayOfMonth}`);
-
-
-    // Zodiac Algorithm
-    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-        AstroSign = "Capricorn";
-      } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-        AstroSign = "Sagittarius";
-      } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-        AstroSign = "Scorpio";
-      } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-        AstroSign = "Libra";
-      } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-        AstroSign = "Virgo";
-      } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-        AstroSign = "Leo";
-      } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-        AstroSign = "Cancer";
-      } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-        AstroSign = "Gemini";
-      } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-        AstroSign = "Taurus";
-      } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-        AstroSign = "Aries";
-      } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-        AstroSign = "Pisces";
-      } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-        AstroSign = "Aquarius";
-      }
-
-      console.log(AstroSign);
-      userPicked(AstroSign);
-  });
 
 // Modal Functionality
 
